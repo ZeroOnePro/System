dfb6bc93f7103fd88b029b2151953608c509cfb0
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-08-15 21:55:51 +0900

    add submodule computer-architecture

---
+++ git.sh
@@ -0,0 +1,27 @@
+echo "############################################"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#    git pull push auto script ver 1.0     #"
+echo "#                                          #"
+echo "#         written by sungminyou            #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "############################################"
+echo -e "\n"
+echo "*** start pull ***"
+echo -e "\n"
+git pull
+echo -e "\n"
+echo -n "please enter a commit message >> "
+read line
+echo -e "\n"
+git add .
+echo -e "\n"
+git commit -m "$line"
+echo -e "\n"
+git push
+echo -e "\n"
+echo "*** Pushing ended successfully ***"
